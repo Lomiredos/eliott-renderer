cacce2acaa4b0e1a9ef20f38b6ca15ced25e6649
Lomiredos <baudoineliott1@gmail.com>
2026-09-11 13:25:53 +0200

    style: retire les commentaires de Color.hpp

--- include/renderer/Color.hpp
+++ include/renderer/Color.hpp
@@ -5,8 +5,6 @@
 namespace ee::renderer
 {
 
-    // Couleur RGB pour la teinte (color modulation).
-    // Defaut = blanc (255,255,255) => aucune teinte (multiplication par 1).
     struct Color
     {
         std::uint8_t r = 255;
